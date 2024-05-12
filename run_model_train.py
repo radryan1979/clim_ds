aeba96c5e4d809ef8925d6bd7d8cb16b1b33b051
Ryan Eagan <ryan.eagan@icloud.com>
2024-05-12 14:54:48 -0400

    Fixed some stats and paths.

--- run_model_train.py
+++ run_model_train.py
@@ -4,6 +4,7 @@ import torch.optim as optim
 import torch.utils.data as data
 from torch.utils.data import DataLoader, sampler, TensorDataset
 from torch.utils.data import sampler
+import torchvision.utils as utils
 
 import torch.nn.functional as F
 
@@ -20,6 +21,8 @@ from scipy.interpolate import interp2d
 from scipy.interpolate import RectBivariateSpline
 from scipy.ndimage.filters import gaussian_filter
 
+from data_utils import TrainDatasetFromFolder, ValDatasetFromFolder, display_transform
+
 from pathlib import Path
 
 from time import time
@@ -31,13 +34,21 @@ from tqdm import tqdm
 
 import os
 
+import pandas as pd
+
+import pytorch_ssim
+
+# import warnings
+# warnings.filterwarnings('ignore')
+
 #-------------------------------------------#
 
 UPSCALE_FACTOR = 2
-NUM_EPOCHS = 1
+NUM_EPOCHS = 2
 
-base_folder = '/Volumes/DataDrive/clim_model_runs'
-run_number = 1
+base_folder = '/Volumes/DataDrive/clim_model_runs/'
+out_path = '/Volumes/DataDrive/clim_model_runs/'
+run_number = 2
 
 
 #-------------------------------------------#
@@ -145,10 +156,66 @@ for epoch in range(1, NUM_EPOCHS + 1):
         ))
 
     netG.eval()
+    
+    with torch.no_grad():
+        val_bar = tqdm(loader_val)
+        valing_results = {'mse': 0, 'ssims': 0, 'psnr': 0, 'ssim': 0, 'batch_sizes': 0}
+        val_images = []
+        for val_lr, val_hr in val_bar:
+            batch_size = val_lr.size(0)
+            valing_results['batch_sizes'] += batch_size
+            lr = val_lr
+            hr = val_hr
+            if torch.cuda.is_available():
+                lr = lr.cuda()
+                hr = hr.cuda()
+            sr = netG(lr)
 
+            batch_mse = ((sr - hr) ** 2).data.mean()
+            valing_results['mse'] += batch_mse * batch_size
+            batch_ssim = pytorch_ssim.ssim(sr, hr).item()
+            valing_results['ssims'] += batch_ssim * batch_size
+            valing_results['psnr'] = 10 * np.log10((hr.max()**2) / (valing_results['mse'] / valing_results['batch_sizes']))
+            valing_results['ssim'] = valing_results['ssims'] / valing_results['batch_sizes']
+            val_bar.set_description(
+                desc='[converting LR images to SR images] PSNR: %.4f dB SSIM: %.4f' % (
+                    valing_results['psnr'], valing_results['ssim']
+                )
+            )
 
-modelg_file = os.path.join(base_folder,f'g_model_run_{run_number}.pth')
-modeld_file = os.path.join(base_folder,f'd_model_run_{run_number}.pth')
+            val_images.extend(
+                [display_transform()(hr.data.cpu().squeeze(0)), 
+                    display_transform()(sr.data.cpu().squeeze(0))]
+            )
+        val_images = torch.stack(val_images)
+        val_images = torch.chunk(val_images, val_images.size(0) // 15)
+        val_save_bar = tqdm(val_images, desc='[saving training results]')
+        index = 1
+        for image in val_save_bar:
+            image = utils.make_grid(image, nrow=3, padding=5)
+            utils.save_image(image, out_path + 'epoch_%d_index_%d.png' % (epoch, index), padding=5)
+            index += 1
 
-torch.save(netG.state_dict(), modelg_file)
-torch.save(netD.state_dict(), modeld_file)
+    # save model parameters
+    gmodel = os.path.join(out_path, f'epochs/netG_epoch_{run_number}_{epoch}.pth')
+    dmodel = os.path.join(out_path, f'epochs/netD_epoch_{run_number}_{epoch}.pth')
+    
+    torch.save(netG.state_dict(), gmodel)
+    torch.save(netD.state_dict(), dmodel)
+    # save loss\scores\psnr\ssim
+    results['d_loss'].append(running_results['d_loss'] / running_results['batch_sizes'])
+    results['g_loss'].append(running_results['g_loss'] / running_results['batch_sizes'])
+    results['d_score'].append(running_results['d_score'] / running_results['batch_sizes'])
+    results['g_score'].append(running_results['g_score'] / running_results['batch_sizes'])
+    results['psnr'].append(valing_results['psnr'])
+    results['ssim'].append(valing_results['ssim'])
+
+    if epoch % 10 == 0 and epoch != 0:
+        stat_path = os.path.join(out_path,"statistics/")
+        print(stat_path)
+        data_frame = pd.DataFrame(
+            data = {'Loss_D': results['d_loss'], 'Loss_G': results['g_loss'], 'Score_D': results['d_score'], 
+                    'Score_G': results['g_score'], 'PSNR': results['psnr'], 'SSIM': results['ssim'], }, 
+                    index=range(1, epoch+1)
+        )
+        data_frame.to_csv(stat_path + 'srf_' + str(UPSCALE_FACTOR) + '_train_results.csv', index_label='Epoch')
